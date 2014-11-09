f6bcbb0b75877a836171e2cf999b1add4b600242
Ihsorak <waspleader5634@yahoo.com>
2014-11-08 20:08:59 -0700

    Adding Level 4
    Bagel Battle

--- levels.lua
+++ levels.lua
@@ -82,6 +82,39 @@ function game:loadLevel(num)
 
 	end
 
+	if num == 4 then
+
+		--backgrounds
+		--{x, y, scalex, scaley, img}
+		self:addEnt(background, {450,415,0.15,0.15,"mountains1"})--Mountain1 needs 45 more vertical
+		self:addEnt(background, {0,370,0.15,0.15,"mountains2"})
+		self:addEnt(background, {0,400,0.15,0.15,"tree2"}) --tree2 are short and need 60 extra vertical
+		self:addEnt(background, {150,340,0.15,0.15,"tree1"})
+		self:addEnt(background, {420,340,0.15,0.15,"tree1"})
+		self:addEnt(background, {80,80,0.15,0.15,"cloud2"})
+		self:addEnt(background, {700,200,0.15,0.15,"cloud1"})
+		self:addEnt(background, {630,300,0.15,0.15,"cloud1"})
+		self:addEnt(background, {280,450,0.15,0.3,"pillar"})
+		self:addEnt(background, {520,450,0.15,0.3,"pillar"})
+		self:addEnt(background, {gameWidth-100,400,0.15,0.15,"tree2"})
+
+		--platforms
+		--{x, y, width, height, unique number, img, scalex, scaley, x-offset, y-offset}
+		self:addEnt(platform,{-100, gameHeight-40, gameWidth+200, 40, 1, "ground", 0.8, 0.5, 0, 40})
+		self:addEnt(platform,{200, 450, 250, 20, 2, "platform", 0.2, 0.15, 100, 10})
+		self:addEnt(platform,{430, 450, 207, 20, 2, "platform", 0.2, 0.15, 100, 10})
+
+		--enemies
+		--{x, y, hasParachute}
+		self:addEnt(breadman,{gamewidth-100, gameHeight - 40, false})
+		numEnemies = 1
+		numSpecial = 0
+
+		--goes in reverse order because i'm lazy
+		spawnQueue = {"bagel"}
+
+	end
+
 end
 
 function game:nextLevel()
